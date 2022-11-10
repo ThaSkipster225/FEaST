747609b8361baec8b4b7da95a65ae9fb65566657
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2022-11-10 12:17:45 -0500

    Added new folder with all of the new Vue Stuff
    This one works now, testing here to make sure it does

---
+++ new_feast/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+  <div id="app">
+    <CoinForm />
+  </div>
+</template>
+
+<script>
+import CoinForm from './components/CoinForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CoinForm
+  }
+}
+</script>
+
+<style lang="css">
+  @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+</style>
