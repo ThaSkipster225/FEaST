c9bbac92a410feb7de6b994b58403d0e9fcd632e
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2022-11-11 11:35:32 -0500

    Fixed Issues
    Added our CSS, now site looks like ours again

--- new_feast/src/App.vue
+++ new_feast/src/App.vue
@@ -15,7 +15,3 @@ export default {
   }
 }
 </script>
-
-<style lang="css">
-  @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-</style>
--- new_feast/src/main.js
+++ new_feast/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import './assets/css/FEaST_CSS.css'
 
 const app = createApp(App)
 app.use(router)
