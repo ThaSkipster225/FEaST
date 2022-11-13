7f640bbadfd0f61a6a92f58adfbb751608edc6da
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2022-11-13 08:45:19 -0500

    Trying to fix component issues

--- new_feast/src/views/AboutView.vue
+++ new_feast/src/views/AboutView.vue
@@ -1,5 +1,18 @@
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+    name: "AboutView",
+    Components: {
+        HelloWorld,
+    }
+}
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloWorld />
   </div>
 </template>
