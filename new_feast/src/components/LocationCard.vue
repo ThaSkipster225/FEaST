61610afe01387411d18bf87709fd6067fa1e49b5
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2022-11-14 18:57:49 -0500

    Testing
    Removed props

--- new_feast/src/components/LocationCard.vue
+++ new_feast/src/components/LocationCard.vue
@@ -2,11 +2,11 @@
 <template>
   <div class="col-lg-3 col-sm-6">
     <div class="food-location">
-      <h3>{{ LocationName }}</h3>
-      <a href="CompSci.html"><img src="../assets/Images/FreshTwist/FreshTwistImage.jpg" alt="{{ LocationName }}" class="w-100">
+      <h3>Fresh Twist</h3>
+      <a href="CompSci.html"><img src="../assets/Images/FreshTwist/FreshTwistImage.jpg" alt="Fresh Twist" class="w-100">
       <div class="location-overlay">
         <div>
-          <h3>{{ LocationName }}</h3>
+          <h3>Fresh Twist</h3>
           <h6>Located in the Weinstein building, Pretzelmaker has been making fresh baked and handcrafted pretzels since 1991. If you ever get a craving for something salty, you can’t go wrong going with Freshtwist!</h6>
         </div>
       </div>
@@ -17,8 +17,6 @@
 
 <script>
 export default {
-    props: {
-      LocationName: String
-    }
+    
 }
 </script>
