1b7afed08af16c520ab3321931ab0c5a00a212ba
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2022-12-02 13:02:56 -0500

    Testing out AboutVie

--- new_feast/src/views/AboutView.vue
+++ new_feast/src/views/AboutView.vue
@@ -1,13 +1,29 @@
-<script>
+<script setup>
 
 import ItemCard from '@/components/ItemCard.vue';
 
-export default {
-    name: "AboutView",
-    components: {
-        ItemCard
-    }
-}
+import app from '../firebaseInit';
+import 'firebase/firestore'
+import { getFirestore, collection, getDocs} from 'firebase/firestore';
+
+        // Reference to Firestore       
+const db = getFirestore(app)
+
+const querySnapshot = await getDocs(collection(db, "Restaurants"));
+console.log(querySnapshot.docs.data())
+// querySnapshot.forEach((doc) => {
+//     console.log(doc.data().Location);
+//     // const data = {
+
+//     // }
+// });
+
+// export default {
+//     name: "AboutView",
+//     components: {
+//         ItemCard
+//     }
+// }
 </script>
 
 <template>
@@ -18,3 +34,18 @@ export default {
     :price="'5.79'"/>
   </div>
 </template>
+  <!-- <template>
+    <Suspense>
+      <template #Default>
+      <div class="about">
+        <h1>This is an about page, which is being used to test out features</h1>
+        <ItemCard
+        :name="'Pretzel Bites'"
+        :price="'5.79'"/>
+      </div>
+      </template>
+      <template #Fallback>
+        <h1> Test </h1>
+      </template>
+    </Suspense>
+  </template> -->
