2b4614d27f0a2c585d5c49724ce482c7715398f1
AceofCardz02 <austinscheetz@gmail.com>
2022-12-04 17:00:00 -0500

    Loaded everything into Buckstop Vue

--- new_feast/src/views/BuckStop.vue
+++ new_feast/src/views/BuckStop.vue
@@ -20,11 +20,21 @@ export default {
     },
     data() {
       return {
-        slices: []
+        slices: [],
+        pizzas: [],
+        breakfasts: [],
+        grilled: [],
+        sides: [],
+        beverages: []
       }
     },
     created () {
       this.getPizza()
+      this.getPies()
+      this.getBreakfasts()
+      this.getLunches()
+      this.getSides()
+      this.getBeverages()
     },
     methods: {
       async getPizza() {
@@ -39,6 +49,76 @@ export default {
           console.log(doc.data())
         })
       
+      },
+
+      async getPies() {
+        // query to get all docs in 'By The Slice' collection
+        // const querySnap = await getDocs(query(collectionReference(db, 'Restaurants')));
+
+        const querySnap = await getDocs(query(collection(db, "/Restaurants/4eSPxsdAIQlyvrBh2wbA/Whole Pizza")));
+
+        // Add each doc to 'slices' array
+        querySnap.forEach((doc) => {
+          this.pizzas.push(doc.data())
+          console.log(doc.data())
+        })
+      
+      },
+
+      async getBreakfasts() {
+        // query to get all docs in 'By The Slice' collection
+        // const querySnap = await getDocs(query(collectionReference(db, 'Restaurants')));
+
+        const querySnap = await getDocs(query(collection(db, "/Restaurants/4eSPxsdAIQlyvrBh2wbA/Breakfast")));
+
+        // Add each doc to 'slices' array
+        querySnap.forEach((doc) => {
+          this.breakfasts.push(doc.data())
+          console.log(doc.data())
+        })
+      
+      },
+
+      async getLunches() {
+        // query to get all docs in 'By The Slice' collection
+        // const querySnap = await getDocs(query(collectionReference(db, 'Restaurants')));
+
+        const querySnap = await getDocs(query(collection(db, "/Restaurants/4eSPxsdAIQlyvrBh2wbA/Lunch")));
+
+        // Add each doc to 'slices' array
+        querySnap.forEach((doc) => {
+          this.grilled.push(doc.data())
+          console.log(doc.data())
+        })
+      
+      },
+
+      async getSides() {
+        // query to get all docs in 'By The Slice' collection
+        // const querySnap = await getDocs(query(collectionReference(db, 'Restaurants')));
+
+        const querySnap = await getDocs(query(collection(db, "/Restaurants/4eSPxsdAIQlyvrBh2wbA/Sides and Extras")));
+
+        // Add each doc to 'slices' array
+        querySnap.forEach((doc) => {
+          this.sides.push(doc.data())
+          console.log(doc.data())
+        })
+      
+      },
+
+      async getBeverages() {
+        // query to get all docs in 'By The Slice' collection
+        // const querySnap = await getDocs(query(collectionReference(db, 'Restaurants')));
+
+        const querySnap = await getDocs(query(collection(db, "/Restaurants/4eSPxsdAIQlyvrBh2wbA/Beverages")));
+
+        // Add each doc to 'slices' array
+        querySnap.forEach((doc) => {
+          this.beverages.push(doc.data())
+          console.log(doc.data())
+        })
+      
       }
     },
 }
@@ -48,6 +128,31 @@ export default {
   <div class="about">
     <h1>By The Slice</h1>
     <ItemCard v-for="value in slices" :key="value.Name"
+    :name="value.Name"
+    :price="value.Price"
+    />
+    <h1>Whole Pies</h1>
+    <ItemCard v-for="value in pizzas" :key="value.Name"
+    :name="value.Name"
+    :price="value.Price"
+    />
+    <h1>Breakfast</h1>
+    <ItemCard v-for="value in breakfasts" :key="value.Name"
+    :name="value.Name"
+    :price="value.Price"
+    />
+    <h1>Lunch from the Grill</h1>
+    <ItemCard v-for="value in grilled" :key="value.Name"
+    :name="value.Name"
+    :price="value.Price"
+    />
+    <h1>Sides and Extra Fixings</h1>
+    <ItemCard v-for="value in sides" :key="value.Name"
+    :name="value.Name"
+    :price="value.Price"
+    />
+    <h1>Beverages</h1>
+    <ItemCard v-for="value in beverages" :key="value.Name"
     :name="value.Name"
     :price="value.Price"
     />
