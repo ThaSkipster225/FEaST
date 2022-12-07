61b5a2eca111331e8b99fe5f446eb7ff22afb6ef
anselmiscolumna <57639912+anselmiscolumna@users.noreply.github.com>
2022-12-07 16:21:27 -0500

    Updated Register modal
    Includes meal plan radios and saves meal plan values to user information

--- new_feast/src/components/RegisterModal.vue
+++ new_feast/src/components/RegisterModal.vue
@@ -11,53 +11,61 @@
           <form @submit.prevent="Register">
             <!-- Student ID -->
             <div class="studentID">
-              <label for="InputFirstName" class="form-label">Student ID</label>
-              <input class="form-control" v-model="studID" id="exampleInputPassword2">
+              <label for="InputStudentID" class="form-label">Student ID</label>
+              <input class="form-control" v-model="studID" id="InputStudentID">
             </div>
 
             <!-- First Name -->
             <div class="firstName">
               <label for="InputFirstName" class="form-label">First Name</label>
-              <input class="form-control" v-model="firstName" id="exampleInputPassword2">
+              <input class="form-control" v-model="firstName" id="inputFirstName">
             </div>
 
             <!-- Last Name -->
             <div class="lastName">
               <label for="InputLastName" class="form-label">Last Name</label>
-              <input class="form-control" v-model="lastName" id="exampleInputPassword2">
+              <input class="form-control" v-model="lastName" id="InputLastName">
             </div>
 
             <!-- Email -->
             <div class="mb-2 emailInput">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" v-model="email" id="exampleInputEmail2"
+              <label for="userEmail" class="form-label">Email address</label>
+              <input type="email" class="form-control" v-model="email" id="userEmail"
                 aria-describedby="emailHelp">
               <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
 
             <!-- Password -->
             <div class="passwordInput">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" v-model="password" id="exampleInputPassword2">
+              <label for="userPassword" class="form-label">Password</label>
+              <input type="password" class="form-control" v-model="password" id="userPassword">
             </div>
 
             <!-- check box options for meal plan -->
-            <div class="form-check form-check-inline" >
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" v-model="option1">
-              <label class="form-check-label" for="inlineRadio1">Meal Plan A</label>
+            <div class="Meal-Plan-Radios">
+              <label class="form-label" for="inlineRadio1">Meal Plan</label>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="RadioOption1" id="Meal1" value="160"
+                  v-model="option" checked>
+                <label class="form-check-label" for="inlineRadio1">Meal Plan A: 20-Meals (160 Snake Bites)</label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="RadioOption2" id="Meal2" value="120"
+                  v-model="option">
+                <label class="form-check-label" for="inlineRadio2">Meal Plan B: 15-Meals (120 Snake Bites)</label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="RadioOption3" id="Meal3" value="80"
+                  v-model="option">
+                <label class="form-check-label" for="inlineRadio3">Meal Plan C: 10-Meals (80 Snake Bites)</label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="RadioOption4" id="Meal4" value="40"
+                  v-model="option">
+                <label class="form-check-label" for="inlineRadio3">Meal Plan D: 5-Meals (40 Snake Bites)</label>
+              </div>
             </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" v-model="option2">
-              <label class="form-check-label" for="inlineRadio2">Meal Plan B</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" v-model="option3">
-              <label class="form-check-label" for="inlineRadio3">Meal Plan C</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" v-model="option4">
-              <label class="form-check-label" for="inlineRadio3">Meal Plan D</label>
-            </div>
+
 
             <!-- Modal Footer -->
             <div class="modal-footer">
@@ -93,8 +101,8 @@ const email = ref('')
 const password = ref('')
 const firstName = ref('')
 const lastName = ref('')
-const studID = ref('')
-const option1 = ref('')
+const studID = ref()
+const option = ref()
 
 const auth = getAuth(firebase)
 const db = getFirestore(firebase)
@@ -102,17 +110,18 @@ const db = getFirestore(firebase)
 const Register = () => {
   createUserWithEmailAndPassword(auth, email.value, password.value)
     .then(() => {
-      const docRef = setDoc(doc(db, "users", auth.currentUser.uid),{
+      const docRef = setDoc(doc(db, "users", auth.currentUser.uid), {
         firstName: firstName.value,
         lastName: lastName.value,
         email: email.value,
-        StudentID: studID.value
+        StudentID: parseInt(studID.value),
+        SnakeBites: parseInt(option.value)
       })
       console.log("Document written with ID: ", docRef.id)
-      console.log(option1)
+      console.log(option.value)
       alert("Register complete!")
     })
-    .catch(err => console.log('Something went wrong with adding user to firestore: ', err, option1.value));
+    .catch(err => console.log('Something went wrong with adding user to firestore: ', err.message));
 }
 
 </script>
