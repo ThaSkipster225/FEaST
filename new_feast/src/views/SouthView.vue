efcfe3151e2c170ddf108db588f2afc1d24de3f1
AceofCardz02 <austinscheetz@gmail.com>
2022-12-06 23:23:17 -0500

    South View Finished

--- new_feast/src/views/SouthView.vue
+++ new_feast/src/views/SouthView.vue
@@ -27,7 +27,17 @@ export default {
         Fuel: [],
         Salads: [],
         Takeoffs: [],
-        Downs: []
+        Downs: [],
+        Grill: [],
+        Alts: [],
+        Breakfast: [],
+        Coffees: [],
+        Espressos: [],
+        Extras: [],
+        Frapps: [],
+        Iced: [],
+        TuSalads: [],
+        Sandwiches: []
       }
     },
     created () {
@@ -52,6 +62,26 @@ export default {
         const queryTakeoff = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Smooth Takeoff")));
         // Get Rocket Cones
         const queryDown = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Splash Down")));
+        // Get Rocket Cones
+        const queryGrill = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Rocket Cones")));
+        // Get Craters
+        const queryAlt = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Rocket Fuel")));
+        // Get Rocket Cones
+        const queryBreakfast = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Shaker Salads")));
+        // Get Craters
+        const queryCoffee = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Smooth Takeoff")));
+        // Get Rocket Cones
+        const queryEspress = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Splash Down")));
+        // Get Rocket Cones
+        const queryXTRA = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Rocket Cones")));
+        // Get Craters
+        const queryFrapps = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Rocket Fuel")));
+        // Get Rocket Cones
+        const queryIced = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Shaker Salads")));
+        // Get Craters
+        const queryTuSalad = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Smooth Takeoff")));
+        // Get Rocket Cones
+        const querySandos = await getDocs(query(collection(db, "/Restaurants/QWTJSoV1l1994PCeLjQK/Splash Down")));
 
 
         // Add each Pretzel doc to 'Pretzels' array
@@ -94,6 +124,56 @@ export default {
           this.Downs.push(doc.data())
         });
 
+        // Add each Beverage doc to 'Beverages' array
+        queryGrill.forEach((doc) => {
+          this.Grill.push(doc.data())
+        });
+
+        // Add each Breakfast Sandwich doc to 'Breakfast' array
+        queryAlt.forEach((doc) => {
+          this.Alts.push(doc.data())
+        });
+
+        // Add each Beverage doc to 'Beverages' array
+        queryBreakfast.forEach((doc) => {
+          this.Breakfast.push(doc.data())
+        });
+
+        // Add each Breakfast Sandwich doc to 'Breakfast' array
+        queryCoffee.forEach((doc) => {
+          this.Coffees.push(doc.data())
+        });
+
+        // Add each Beverage doc to 'Beverages' array
+        queryEspress.forEach((doc) => {
+          this.Espressos.push(doc.data())
+        });
+
+        // Add each Beverage doc to 'Beverages' array
+        queryXTRA.forEach((doc) => {
+          this.Extras.push(doc.data())
+        });
+
+        // Add each Breakfast Sandwich doc to 'Breakfast' array
+        queryFrapps.forEach((doc) => {
+          this.Frapps.push(doc.data())
+        });
+
+        // Add each Beverage doc to 'Beverages' array
+        queryIced.forEach((doc) => {
+          this.Iced.push(doc.data())
+        });
+
+        // Add each Breakfast Sandwich doc to 'Breakfast' array
+        queryTuSalad.forEach((doc) => {
+          this.TuSalads.push(doc.data())
+        });
+
+        // Add each Beverage doc to 'Beverages' array
+        querySandos.forEach((doc) => {
+          this.Sandwiches.push(doc.data())
+        });
+
       }
     },
 }
@@ -106,6 +186,8 @@ export default {
       <ul>
         <li><a href="#happyplace">The Happy Place</a></li>
         <li><a href="#mocketship">Mocketships</a></li>
+        <li><a href="#grillmaster">Grillmasters</a></li>
+        <li><a href="#tutus">Tutus' Cyber Cafe</a></li>
       </ul>
     </nav>
             <h2>Ala Carte Menu</h2>
@@ -174,6 +256,92 @@ export default {
                         :price="value.Price"
                     />
                 </div>
-    </div>
+        </div>
     
+        <div class="Grillmasters">
+        <h1>Grillmasters</h1>
+            <h2>Grill</h2>
+                <div class="row">
+                    <ItemCard v-for="value in Grill" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+        </div>
+
+        <div class="Tutus">
+        <h1>Tutus' Cyber Cafe</h1>
+            <h2>Alternatives</h2>
+                <div class="row">
+                    <ItemCard v-for="value in Alts" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+
+            <h2>Breakfasts</h2>
+                <div class="row">
+                    <ItemCard v-for="value in Breakfast" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+
+                <h2>Coffees</h2>
+                <div class="row">
+                    <ItemCard v-for="value in Coffees" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+
+            <h2>Espressos</h2>
+                <div class="row">
+                    <ItemCard v-for="value in Espressos" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+
+                <h2>Extras</h2>
+                <div class="row">
+                    <ItemCard v-for="value in Extras" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+
+            <h2>Frappechinos</h2>
+                <div class="row">
+                    <ItemCard v-for="value in Frapps" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+
+            <h2>Iced Coffees</h2>
+                <div class="row">
+                    <ItemCard v-for="value in Iced" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+
+            <h2>Salads</h2>
+                <div class="row">
+                    <ItemCard v-for="value in TuSalads" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+
+            <h2>Sandwiches</h2>
+                <div class="row">
+                    <ItemCard v-for="value in Sandwiches" :key="value.Name"
+                        :name="value.Name"
+                        :price="value.Price"
+                    />
+                </div>
+        </div>
+
 </template>
