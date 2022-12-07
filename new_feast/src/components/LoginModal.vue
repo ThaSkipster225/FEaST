1ef88f57b310937760ee357de4f82c814114e062
anselmiscolumna <57639912+anselmiscolumna@users.noreply.github.com>
2022-12-07 01:06:58 -0500

    Just some slight editing

--- new_feast/src/components/LoginModal.vue
+++ new_feast/src/components/LoginModal.vue
@@ -3,24 +3,24 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Log into your FEaST Account</h5>
+          <h5 class="modal-title" id="ModalLabel">Log into your FEaST Account</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <form @submit.prevent="Login">
             <div class="mb-2 emailInput">
               <label for="InputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" v-model="email" id="exampleInputEmail1"
+              <input type="email" class="form-control" v-model="email" id="InputEmail1"
                 aria-describedby="emailHelp">
               <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="passwordInput">
               <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" v-model="password" id="exampleInputPassword1">
+              <input type="password" class="form-control" v-model="password" id="InputPassword1">
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <input type="submit" class="btn btn-primary" value="Login" placeholder="Log In">
+              <input type="submit" class="btn btn-primary" value="Login" @click="onSubmit" placeholder="Log In">
               <button type="button" class="btn btn-primary m-2" data-bs-toggle="modal" data-bs-target="#RegisterModal">Register Here!</button>
             </div>
           </form>
@@ -30,16 +30,6 @@
   </div>
 
 </template>
-
-<script>
-export default{
-  data(){
-    return{
-      userInfo: []
-    }
-  }
-}
-</script>
 
 <script setup>
 import { ref } from 'vue';
@@ -62,11 +52,18 @@ const Login = () => {
        
       console.log(userInfo(profile))
       let user = userInfo(profile)
+
+      alert("Login Successful!")
+
+      return{
+        user
+      }      
     })
     .catch((error) => {
       alert(error.message);
     });
 }
+
 
 function userInfo(doc){
   let firstName = doc.data().firstName
